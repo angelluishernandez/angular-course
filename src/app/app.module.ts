28203bc7ce7000efa1303b2936ad43a3bb6cf121
Ángel Luis Hernández Herrero <angelluishernandezherrero@gmail.com>
2020-04-09 11:14:56 +0200

    first asignment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { WarningAlertComponent } from './warningAlert/warningAlert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
     AppComponent, 
-    ServerComponent
+    ServerComponent, 
+    ServersComponent, 
+    WarningAlertComponent, SuccessAlertComponent
   ],
   imports: [
     BrowserModule,
